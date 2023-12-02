da7593241bd8ffd68ba19ad7ddb364001e4dcfe3
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-12-02 14:09:52 -0800

    Font and grid test

---
+++ src/lib/components/windowCard.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div class="grid-cols-2">
+    <div class="mx-8 rounded-xl outline-2">
+        <h2 class="text-lg">My Window</h2>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1 class="text-3xl font-bold grid text-center mx-auto p-4">Welcome to our Window Insert Store</h1>
-<p>Testing</p>
+<script>
+	import WindowCard from "$lib/components/windowCard.svelte";
+
+</script>
+<h1 class="text-3xl font-bold text-center mx-auto p-4 mt-4">Welcome to our Window Insert Store</h1>
+<div class="grid-flow-col p-3">
+    <div class="columns-3">
+    <div class="mx-auto rounded-xl outline text-center">
+        <div class="shadow-xl p-4">
+            <WindowCard />
+        </div>
+        <div class="shadow-xl p-4">
+            <WindowCard />
+        </div>
+    </div>
+    </div>
+</div>
