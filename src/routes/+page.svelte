2c61ae5fccc610eaeb9b20c0a457267fc6181326
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-12-02 13:24:17 -0800

    Add Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold grid text-center mx-auto p-4">Welcome to our Window Insert Store</h1>
+<p>Testing</p>
