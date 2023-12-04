c8c959b5b696f01c009a034287a260cc9203e392
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-12-04 07:28:46 -0800

    Basic form

--- src/lib/components/windowCard.svelte
+++ src/lib/components/windowCard.svelte
@@ -4,6 +4,10 @@
 
 <div class="mx-auto rounded-xl outline text-center">
     <div class="shadow-xl p-4">
-        My Window
+        <form action="">
+            <input class="outline rounded-sm p-2 m-2" type="text" name="Window" id="window" placeholder="Window i.e north kitchen">
+            <input class="outline rounded-sm p-2 m-2 w-1/2" type="int" name="Width" id="width" placeholder="Width">
+            <input class="outline rounded-sm p-2 m-2 w-1/2" type="int" name="Height" id="height" placeholder="Height">
+        </form>
     </div>
 </div>
